1085818cb8ad2a7b7fd788d5f05b24a06dea99f7
Phillip Whittlesea-Clark <pw.github@thega.me.uk>
2025-10-09 15:16:05 +0100

    Move pre-commit hooks

--- .hooks/correct_exif.sh
+++ .hooks/correct_exif.sh
@@ -9,12 +9,11 @@ COPYRIGHT="© ${YEAR} ${NAME} - All Rights Reserved."
 # List of paths to skip
 # I don't own these images so I don't want to set the copyright as mine
 SKIP_PATHS=(
-  "assets/images/2025-03-28-the-beautiful-world-of-walter-mitty/header.jpg"
-  "assets/images/2025-03-28-the-beautiful-world-of-walter-mitty/the_secret_life_of_walter_mitty_1.jpg"
-  "assets/images/2025-03-28-the-beautiful-world-of-walter-mitty/the_secret_life_of_walter_mitty_2.jpg"
-  "assets/images/2025-06-09-i-am-whittlesea-clark/success_kid.png"
-  "assets/images/2025-06-09-i-am-whittlesea-clark/surprise.png"
-  "assets/images/2025-06-15-long-live-the-lakehouse/iceberg-metadata.png"
+  "content/posts/2025-03-28-the-beautiful-world-of-walter-mitty/the_secret_life_of_walter_mitty_1.jpg"
+  "content/posts/2025-03-28-the-beautiful-world-of-walter-mitty/featured.jpg"
+  "content/posts/2025-06-09-i-am-whittlesea-clark/success_kid.png"
+  "content/posts/2025-06-09-i-am-whittlesea-clark/surprise.png"
+  "content/posts/2025-06-15-long-live-the-lakehouse/iceberg-metadata.png"
 )
 
 for file in $@
